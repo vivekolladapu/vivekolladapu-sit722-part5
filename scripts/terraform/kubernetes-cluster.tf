d067665eab9777b1786fc0732ac7154692853403
vivekolladapu <68852720+vivekolladapu@users.noreply.github.com>
2024-09-26 20:58:19 +1000

    Update kubernetes-cluster.tf

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                = var.app_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.vivek26.name
+  resource_group_name = azurerm_resource_group.vivek28.name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
 
