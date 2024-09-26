f195ef07947e6a50f3847a138ceb99d4679d5f7d
vivekolladapu <68852720+vivekolladapu@users.noreply.github.com>
2024-09-26 20:58:30 +1000

    Update container-registry.tf

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "container_registry" {
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.vivek26.name
+  resource_group_name = azurerm_resource_group.vivek28.name
   location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
